759b051baf5f9fdcd71568b8c5f9c8c91a192bcc
ValCam18 <valeria-camila18@hotmail.com>
2025-03-16 18:51:32 -0500

    se agregó sección categorías y se corrigió menú y pantalla al deslizar

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Happy Code");
+
+function toggleModal() {
+    document.getElementById('modal').classList.toggle('hidden');
+}
+
+function toggleEditModal() {
+    document.getElementById('edit-modal').classList.toggle('hidden');
+}
+
+function toggleDeleteModal() {
+    document.getElementById('delete-modal').classList.toggle('hidden');
+}
